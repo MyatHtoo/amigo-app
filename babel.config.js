e76ed87fd12db13fdaaaa1b2046585ce4a6bbafd
htay-htay-thwe <130959683+htay-htay-thwe@users.noreply.github.com>
2026-01-20 22:12:11 +0700

    delete test-output.css

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', 'nativewind/babel'],
+    presets: [
+      'babel-preset-expo',
+      ['nativewind/babel', { tailwindcssPath: './tailwind.config.js' }]
+    ],
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
   };
 };
